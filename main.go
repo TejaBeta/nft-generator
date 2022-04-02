aea6e9ae3b52664567347ee596268ce788094943
Tejasvi <me@tejathota.dev>
2022-04-03 00:23:20 +0100

    Final changes

--- main.go
+++ main.go
@@ -1,42 +1,99 @@
 package main
 
 import (
+	"flag"
 	"image"
 	"image/draw"
 	"image/jpeg"
 	"image/png"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	var n int
+	flag.IntVar(&n, "n", 0, "Number to start from")
+	flag.Parse()
 	log.SetFormatter(&log.TextFormatter{
 		DisableColors: true,
-		FullTimestamp: true,
+		FullTimestamp: false,
 	})
 
-	layers, err := readLayers("./layers")
+	multiLayers, err := readLayers("./layers")
 	if err != nil {
 		log.Error(err)
 		return
 	}
-	generator(layers)
+	composer(multiLayers, n)
+	// generator(layers)
 }
 
-func readLayers(dir string) ([]string, error) {
+func composer(m [][]string, n int) {
+	counter := 0
+	imageCounter := 0
+	start := time.Now()
+	for _, l0 := range m[0] {
+		for _, l1 := range m[1] {
+			for _, l2 := range m[2] {
+				for _, l3 := range m[3] {
+					for _, la := range m[4] {
+						for _, lb := range m[5] {
+							for _, lc := range m[6] {
+								for _, ld := range m[7] {
+									for _, le := range m[8] {
+										for _, lf := range m[9] {
+											for _, lg := range m[10] {
+												for _, lh := range m[11] {
+													for _, li := range m[12] {
+														_ = []string{l0, l1, l2, l3, la, lb, lc, ld, le, lf, lg, lh, li}
+														if counter > n && counter < (n+1025) {
+															generator([]string{l0, l1, l2, l3, la, lb, lc, ld, le, lf, lg, lh, li}, strconv.Itoa(counter)+".PNG")
+															imageCounter = imageCounter + 1
+														}
+														counter = counter + 1
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	duration := time.Since(start)
+	log.Println("Images Created:", imageCounter)
+	log.Println("Total Duration: ", duration)
+}
+
+func readLayers(dir string) ([][]string, error) {
 	layers := []string{}
+	multiLayers := [][]string{}
 	err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
-		if !f.IsDir() {
-			layers = append(layers, path)
+		if f.Name() != ".DS_Store" && f.Name() != "layers" {
+			if !f.IsDir() {
+				layers = append(layers, path)
+			} else {
+				if len(layers) > 0 {
+					multiLayers = append(multiLayers, layers)
+					layers = nil
+				}
+			}
 		}
 		return nil
 	})
-	return layers, err
+	multiLayers = append(multiLayers, layers)
+	return multiLayers, err
 }
 
-func generator(images []string) {
+func generator(images []string, output string) {
 	var decodedImages = make([]image.Image, len(images))
 
 	for i, img := range images {
@@ -50,14 +107,14 @@ func generator(images []string) {
 		draw.Draw(newImage, img.Bounds(), img, image.ZP, draw.Over)
 	}
 
-	result, err := os.Create("./final/result.jpg")
+	result, err := os.Create("./final/" + output)
 	if err != nil {
 		log.Fatalf("Failed to create: %s", err)
 	}
 
 	jpeg.Encode(result, newImage, &jpeg.Options{Quality: jpeg.DefaultQuality})
 	defer result.Close()
-	log.Info("File created at ./final/result.jpg")
+	log.Printf("%s", output)
 }
 
 func openAndDecode(imgPath string) image.Image {
