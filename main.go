b593e407c22ff83ac61f8195007d46d65dbd66d7
Tejasvi <me@tejathota.dev>
2022-04-02 12:40:35 +0100

    Finalise Logic

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
+	"os"
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -11,4 +17,44 @@ func main() {
 	})
 
 	log.Println("Hello")
+	generator()
+}
+
+func generator() {
+	var images = []string{"bg_0", "edge_0", "edge_1", "edge_2", "a_0", "b_0", "c_0", "d_0", "e_0", "f_0", "g_0", "h_0", "i_0"}
+	var decodedImages = make([]image.Image, len(images))
+
+	for i, img := range images {
+		decodedImages[i] = openAndDecode("./layers/" + img + ".PNG")
+	}
+
+	bounds := decodedImages[0].Bounds()
+	newImage := image.NewRGBA(bounds)
+
+	for _, img := range decodedImages {
+		draw.Draw(newImage, img.Bounds(), img, image.ZP, draw.Over)
+	}
+
+	result, err := os.Create("./final/result.jpg")
+	if err != nil {
+		log.Fatalf("Failed to create: %s", err)
+	}
+
+	jpeg.Encode(result, newImage, &jpeg.Options{jpeg.DefaultQuality})
+	defer result.Close()
+}
+
+func openAndDecode(imgPath string) image.Image {
+	img, err := os.Open(imgPath)
+	if err != nil {
+		log.Fatalf("Failed to open %s", err)
+	}
+
+	decoded, err := png.Decode(img)
+	if err != nil {
+		log.Fatalf("Failed to decode %s", err)
+	}
+	defer img.Close()
+
+	return decoded
 }
